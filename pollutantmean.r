137347587ac8fffb9a271ca61384929170e864b7
ahmed ragab <eng_ahmedhusseinragab@yahoo.com>
2016-06-12 03:49:45 +0200

    new version

--- pollutantmean.r
+++ pollutantmean.r
@@ -1,5 +1,8 @@
 pollutantmean<-function(directory , pollutant , id=1:332){
-        files<-character(length(id))
+        
+        # Vetor to hold file names 
+        
+        files<-character(length(id)) 
 		
         for (i in 1:length(id)){
 		files[i]<-paste(directory,"/",sprintf("%03d",id[i]),".csv",sep="")
@@ -7,10 +10,18 @@ pollutantmean<-function(directory , pollutant , id=1:332){
                 
                 
         }
+        # apply read.csv function to each file name then apply rbind to all dataframes 
+        
         total<-do.call(rbind,lapply(files,read.csv))
-		vector_poll = total[pollutant]
-		
-		ret_value = colMeans(vector_poll,na.rm=TRUE)
-		return (ret_value)
+        
+        # vector to hold pollutant in questions 
+        
+	vector_poll = total[pollutant]
+	
+	# use colMeans function to get Mean for the column 
+	
+	ret_value = colMeans(vector_poll,na.rm=TRUE)
+	
+	return (ret_value)
         
 }
